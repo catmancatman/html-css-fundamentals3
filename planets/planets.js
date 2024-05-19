74e83a653c2164fe141fb8383145aa75d714d39c
catalin <catmanpetreseniuc@yahoo.co.uk>
2024-05-19 04:32:05 +0300

    dom 4 din 9 final

--- planets/planets.js
+++ planets/planets.js
@@ -1,8 +1,24 @@
-let planet = document.getElementById ("redplanet")
+let button = document.getElementById("check")
+button.addEventListener("click", function () {
+    let planet = document.getElementById("redplanet")
 
-planet.innerText = "Alien invasion"
-planet.classList.add("alert")
-planet.classList.add("important")
-//planet.style.color ="red"
-//planet.style.fontWeight ="bold"
+    planet.innerText = "Alien invasion"
+    planet.classList.add("alert")
+    //planet.style.color ="red"
+    //planet.style.fontWeight ="bold"
+    //planet.remove()
+})
+document.getElementById("reset")
+    .addEventListener("click", function () {
+        let planet = document.getElementById("redplanet")
+        planet.innerText = "Nothing to report"
+        planet.classList.remove("alert")
+    })
+
+document.getElementById("greenplanet")
+    .addEventListener("click", function () {
+        document.getElementById("greenplanet").classList.add("alert")
+    })
+
+
 
