45e973c2222458a37001ac8ddf4240edc96ea24a
catalin <catmanpetreseniuc@yahoo.co.uk>
2024-05-19 03:13:32 +0300

    planets

--- planets/planets.js
+++ planets/planets.js
@@ -1,1 +1,8 @@
-document.getElementById("redplanet").innerText ="Alien invasion"
+let planet = document.getElementById ("redplanet")
+
+planet.innerText = "Alien invasion"
+planet.classList.add("alert")
+planet.classList.add("important")
+//planet.style.color ="red"
+//planet.style.fontWeight ="bold"
+
